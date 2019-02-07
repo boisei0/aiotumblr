4b23f3b99771dc3b15384b5a8c60700627419423
Arlena Derksen <arlena@hubsec.eu>
2019-02-08 00:12:51 +0100

    Improved base model

--- aiotumblr_core/models/base.py
+++ aiotumblr_core/models/base.py
@@ -1,18 +1,32 @@
 # encoding=utf-8
 __all__ = ['BaseModel']
 
-from dataclasses import dataclass, asdict
+import logging
+from dataclasses import dataclass, asdict, fields, is_dataclass
 import json
+
+log = logging.getLogger(__name__)
 
 
 @dataclass
 class BaseModel:
-    def serialise(self):
-        json.dumps(asdict(self))
+    def serialise(self) -> str:
+        return json.dumps(asdict(self))
 
     @classmethod
-    def deserialise(cls, **kwargs):
-        raise NotImplemented
+    def deserialise(cls, **kwargs) -> 'BaseModel':
+        _input = {}
+        for field in fields(cls):
+            if field.name in kwargs:
+                if is_dataclass(field.type):
+                    _input[field.name] = field.type.deserialise(**kwargs[field.name])
+                else:
+                    _input[field.name] = field.type(kwargs[field.name])
+            else:
+                log.debug(f'Found defined property {field.name!r} on {cls.__name__!r} that is not supplied.')
+
+        # noinspection PyArgumentList
+        return cls(**_input)
 
     # For those who prefer American English
     serialize = serialise
